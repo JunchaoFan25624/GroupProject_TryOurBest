8540ec7b529f70ff22353e363130fd88ab50e190
nbachynski <bachynsn@sheridancollege.ca>
2026-04-05 12:06:25 -0400

    Updated addCard

--- src/ca/sheridancollege/project/WarPlayer.java
+++ src/ca/sheridancollege/project/WarPlayer.java
@@ -12,11 +12,13 @@ package ca.sheridancollege.project;
  * REPRESENTS A SINGLE PLAYER IN THE WAR GAME. 
  * STRICT 1-TO-1 COMPOSITION WITH WarGroups.
  */
+
 public class WarPlayer extends Player {
 
-    // TODO: DECLARE A PRIVATE WarGroups ATTRIBUTE (E.G., private WarGroups deck;)
+    // DECLARE A PRIVATE WarGroups ATTRIBUTE (E.G., private WarGroups deck;)
     // THIS REPRESENTS THE PLAYER'S PERSONAL HAND/DECK.
-
+    private WarGroups deck;
+    
     public WarPlayer(String name) {
         super(name);
         // TODO: INITIALIZE THE WarGroups OBJECT HERE
@@ -28,11 +30,12 @@ public class WarPlayer extends Player {
      */
     public void addCard(WarCard card) {
         // TODO: IMPLEMENT ADDING CARD LOGIC
+        deck.addCard(card);
     }
 
     @Override
     public void play() {
-        // TODO: IMPLEMENT ANY PLAYER SPECIFIC PLAY LOGIC IF NEEDED.
+        // IMPLEMENT ANY PLAYER SPECIFIC PLAY LOGIC IF NEEDED.
         // IN WAR, THIS MIGHT JUST BE TRIGGERING THEIR DECK TO grabCard(0).
     }
 }
